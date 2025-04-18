bdb0be6c9454e1405be66db28ba19d5e285b16ce
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2025-04-18 23:58:17 +0700

    anal

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
   <UApp>
+    <Analytics />
     <div class="bg-gray-50 w-full max-w-xl m-auto min-h-dvh">
       <AppHeader />
       <AppNav />
